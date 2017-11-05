673868ddffcaa2c18f713cb14c36c3987ebb6438
John Schwartzenburg <jeschwartzenburg@gmail.com>
2017-11-04 22:24:13 -0500

    displays current holdings in textblock via async button method

--- BittrexGUI/HomePage.xaml.cs
+++ BittrexGUI/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BittrexSharp.Domain;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,31 +13,26 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using BittrexSharp;
-using BittrexSharp.Domain;
-using BittrexSharp.Exceptions;
-using BittrexSharp.BittrexOrderSimulation;
 
-
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace BittrexGUI
 {
-
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class HomePage : Page
     {
-        public Bittrex bittrex = new Bittrex("3ef55d86bd234c2cb380edc363fcce62", "b84ebfcf715e470ba3fe5f44f2fde77d");
 
-        public MainPage()
+        
+        public HomePage(BittrexSharp.Bittrex bittrex)
         {
             this.InitializeComponent();
+            syncHoldings(bittrex);
+
         }
 
-        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        private async void syncHoldings(BittrexSharp.Bittrex bittrex)
         {
             var task = bittrex.GetBalances();
             var balances = await task;
@@ -44,6 +40,7 @@ namespace BittrexGUI
 
             //list of currencyBalance objects to print to GUI
             List<CurrencyBalance> coins = new List<CurrencyBalance>();
+
 
             foreach (var coin in holdings)
             {
@@ -53,11 +50,13 @@ namespace BittrexGUI
                 }
             }
 
-            HoldingsBox.Text = "Current Holdings: \n";
+            HoldingBox2.Text = "Current Holdings: ";
 
-            //prints holdings to HoldingsBox
-            foreach (var coin in coins)
-            { HoldingsBox.Text += "Coin: " + coin.Currency + " | Balance: " + coin.Balance.ToString() + "\n";}
+            foreach(var coin in coins)
+            {
+                HoldingBox2.Text += "Coin: " + coin.Currency + " | Balance: " + coin.Balance.ToString() + "\n";
+            }
+
         }
     }
 }
